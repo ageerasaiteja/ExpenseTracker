454606ae32be75c3ef13e1c5bcc714c15c1e28ed
Ageera Saiteja <ageerasaiteja@gmail.com>
2026-04-06 21:27:25 +0530

    login refactored

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,9 @@
-import { Component, inject, effect, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { FormsModule } from '@angular/forms';
-import { LoginService } from './login-service';
 import { UserService } from '../../services/userService';
 import { AlertService } from '../../services/alertService';
+import { AuthService } from '../../services/authService';
 
 @Component({
   selector: 'app-login',
@@ -16,19 +16,13 @@ export class Login {
   password: string = '';
   showPassword = signal(false);
 
-  loginService = inject(LoginService);
-  userService = inject(UserService);
-  alertService = inject(AlertService);
+  userService: UserService = inject(UserService);
+  alertService: AlertService = inject(AlertService);
+  authService: AuthService = inject(AuthService);
 
-  submit() {
-    this.loginService.submit(this.username, this.password);
-  }
-
-  keydown(e: KeyboardEvent) {
-    if (e.key == 'Enter') {
-      this.submit();
-    }
-  }
+  userError: boolean = false
+  passwordError: boolean = false
+  userLogin = this.userService.getUserLogin()
 
   ngOnInit() {
     if (this.userService.getUserLogin()() == true && this.userService.getUserToken()() !== '') {
@@ -36,6 +30,40 @@ export class Login {
       setTimeout(() => {
         window.location.href = '/dashboard';
       }, 2000);
+    }
+  }
+  
+  submit() {
+    this.Submit(this.username, this.password);
+  }
+  
+  async Submit(username: string, password: string){
+    let [status, highlight, message] = await this.authService.validateInput(username, password)
+    if(status == true){
+      this.alertService.success(message, 1500)
+      setTimeout(()=>{
+        window.location.href = "/dashboard"
+      }, 1700)
+      return
+    }
+    this.alertService.error(message, 3000)
+    if(highlight){
+      if(message.startsWith("Username")){
+        this.userError = true
+      }else{
+        this.passwordError = true
+      }
+    }
+  }
+
+  clearError(){
+    this.passwordError = false
+    this.userError = false
+  }
+
+  keydown(e: KeyboardEvent) {
+    if (e.key == 'Enter') {
+      this.submit();
     }
   }
 
